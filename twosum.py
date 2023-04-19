21cf7d1ec61fe40da98b74793bb3257a7a6f00b8
Megha G <130460371+MeghaG04@users.noreply.github.com>
2023-04-19 18:24:59 +0530

    Add files via upload

---
+++ twosum.py
@@ -0,0 +1,14 @@
+nums = [2,7,11,15]
+target = 9
+
+def solution(nums,target):
+    
+
+    for i in range(0,len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i] + nums[j] == target:
+                print([i,j])    #return[i,j]
+
+
+solution(nums,target)           #print(solutions(nums,target))
+            
