e3d70f30b215560cc04c4ef0b30810847964ab64
Megha G <130460371+MeghaG04@users.noreply.github.com>
2023-04-19 18:25:19 +0530

    pattern

---
+++ pattern.py
@@ -0,0 +1,23 @@
+#for i in range(6):
+ #   for j in range(i,6):
+  #      print("*",end="")
+  #  print()
+
+#for j in range(6):
+#    print("*"*j)
+
+#for i in range(6,0,-1):
+ #   for j in range(i):
+  #      print("*",end="")
+   # print()
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+            
