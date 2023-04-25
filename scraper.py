2471ba57dcb97a495f25ab4fa5407da1e5775dd8
Megha G <130460371+MeghaG04@users.noreply.github.com>
2023-04-25 18:06:18 +0530

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+URL = "https://www.cet.ac.in/head-of-departments/"
+req = requests.get(URL)
+source_code = req.content
+soup = BeautifulSoup(source_code,'lxml')
+table = soup.find("table")
+rows = table.find_all('tr')
+rows = rows[1:]
+for row in rows:
+	columns = row.find_all("td")
+	hod_name = columns[0].text
+	department = columns[1].text
+	print(hod_name,department)
+
